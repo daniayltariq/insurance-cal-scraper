51c21aaa4ccfa9a1cd3f96ba6edc9a2f96095374
WAHHAB <daniyaldottariq@gmail.com>
2023-08-01 21:33:47 +0500

    update

--- scrap.php
+++ scrap.php
@@ -1,9 +1,9 @@
 <?php
 
-ini_set('display_errors', 1);
+ini_set('display_errors', 0);
 ini_set('max_execution_time', 0);
 ini_set('memory_limit', -1);
-error_reporting(E_ALL);
+error_reporting(0);
 
 
 require_once (__DIR__ . '/vendor/autoload.php');
