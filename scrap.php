e978478a77ced4529d7f75d0d81e45ae354ce857
WAHHAB <daniyaldottariq@gmail.com>
2023-08-08 12:09:18 +0500

    update

--- scrap.php
+++ scrap.php
@@ -313,7 +313,7 @@ include_once('simple_html_dom.php');
 
     foreach ($races as $key => $race){
 
-        if(trim($race) == 'Spanischer Mastiff'){
+        if(trim($race) == 'Zwergspitz'){
             $needle = true;
         }
 
